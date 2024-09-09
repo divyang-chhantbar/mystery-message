2dda0db81f726530db699cbab532d52391fba9cd
divyang-chhantbar <divyangchhantbar@gmail.com>
2024-09-09 15:38:10 +0530

    components imported from shadcn

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+'use client'
+import React from 'react'
+import Link from 'next/link'
+import { useSession ,signOut } from 'next-auth/react'
+import { User } from 'next-auth'
+import {Button} from './ui/button'
+
+function Navbar () {
+    const {data: session} = useSession()
+    // yaha se to hume data k andar session mila ya nahi voh pata chalega 
+    // baaki sari information about the session jo hogi voh hume User k andar mil jayegi
+
+    const user:User = session?.user
+    return (
+        <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
+          <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+            <a href="#" className="text-xl font-bold mb-4 md:mb-0">
+              True Feedback
+            </a>
+            {session ? (
+              <>
+                <span className="mr-4">
+                  Welcome, {user.username || user.email}
+                </span>
+                <Button onClick={() => signOut()} className="w-full md:w-auto bg-slate-100 text-black" variant='outline'>
+                  Logout
+                </Button>
+              </>
+            ) : (
+              <Link href="/sign-in">
+                <Button className="w-full md:w-auto bg-slate-100 text-black" variant={'outline'}>Login</Button>
+              </Link>
+            )}
+          </div>
+        </nav>
+      );
+    }
+    export default Navbar;
