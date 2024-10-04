8d8de82690cda99fae5bbcdb62c4959d845b2dbc
divyang-chhantbar <divyangchhantbar@gmail.com>
2024-10-04 16:54:50 +0530

    Add SMTP transporter for sending emails

---
+++ src/lib/smtpTransporter.ts
@@ -0,0 +1,12 @@
+// lib/smtpTransporter.ts
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAIL_USER, // Your Gmail address
+    pass: process.env.GMAIL_PASS, // Your App Password from Gmail
+  },
+});
+
+export default transporter;
